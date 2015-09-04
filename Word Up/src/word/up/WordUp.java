db1e0919b7ecdf19f3b643d4c44424f255c7638f
Smokie Lee <xtoq@users.noreply.github.com>
2015-09-03 20:56:21 -0500

    0.2
    - Initialize the array list as a new array instead of as null...derp.
    - assign values
    - set random number
    - print message associated with random number

--- Word Up/src/word/up/WordUp.java
+++ Word Up/src/word/up/WordUp.java
@@ -6,27 +6,26 @@
 package word.up;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  *
  * @author Smokie Lee
  */
 public class WordUp {
-  // initialize variables
-  // arrayList, randomNumber, printMessage
   private ArrayList arrayList;
   private Integer randomNumber;
   private String printMessage;
   
   // constructors
   public WordUp() {
-    this.arrayList = null;
+    this.arrayList = new ArrayList<>();
     this.randomNumber = 0;
     this.printMessage = "";
   }
   
-  public WordUp(String variable) {
-  }
+//  public WordUp(String variable) {
+//  }
   
   // getters
   // getArrayList, getRandomNumber, getPrintMessage
@@ -38,14 +37,15 @@ public class WordUp {
     return randomNumber;
   }
   
-  public String printMessage() {
+  public String getPrintMessage() {
     return printMessage;
   }
   
   // setters
   // setArrayList, setRandomNumber, setPrintMessage
 //  public void setArrayList(int numMessages,ArrayList<String> messageListName) {
-  public void setArrayList(String x) {
+//  public void setArrayList(String x) {
+  public void setArrayList() {
     // set/put values into the array list
 //    numMessages = 3;
 //    for (int i = 1; i <= numMessages; i++) { // Start the count at 1 instead of 0.
@@ -54,19 +54,25 @@ public class WordUp {
 //      messageListName.add("Second");
 //      messageListName.add("Third");
 //    }
-    arrayList.add(x);
+//    this.arrayList.add(x);
+    this.arrayList.add("First");
+    this.arrayList.add("Second");
   }
   
   public void setRandomNumber() {
     // set random number after computing it
   }
   
-  public void setPrintMessage() {
+  public void setPrintMessage(int messageNumber, ArrayList<String> arrayList) {
     // set print message value
-    int order = 1; // Counter for the numbered list.
-    for (Object message : arrayList) { // Enhanced for loop is more efficient here.
-      System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
-    }
+    System.out.printf("%s%n",arrayList.get(messageNumber));
+    this.printMessage = null;
+    
+//    // print array list in readable format
+//    int order = 1; // Counter for the numbered list.
+//    for (Object message : arrayList) { // Enhanced for loop is more efficient here.
+//      System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
+//    }
   }
   
 //  // other methods
@@ -83,24 +89,38 @@ public class WordUp {
    */
   public static void main(String[] args) {
     // create new object(s)
-//    WordUp a = new WordUp();
+//    WordUp w = new WordUp();
     ArrayList l = new ArrayList();
 
     // set array list
-//    a.setArrayList("first");
-    l.add("first");
-    l.add("second");
-    l.add("third");
+    l.add("puppy");
+    l.add("kitten");
+    l.add("duckling");
+    l.add("laser");
+    l.add("larvae");
     
     // generate random number
+    int s = l.size();
+    int a = 0; // Lower bound is the first value. First index in an array list is always 0.
+    int b = s - 1; // Upper bound is the size of the array list - 1.
+    int r = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
+//    int r = (int) (Math.random() * (l.size() + 1)); // Simplified equation. Probably don't want to use this.
     
     // print array list message
-//    a.setPrintMessage();
-//    System.out.printf("%s",l);
+    
+    // debugging
+    System.out.println("RESULTS");
+    
     int order = 1; // Counter for the numbered list.
     for (Object message : l) { // Enhanced for loop is more efficient here.
       System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
     }
+    
+//    System.out.println("Random number = " + r);
+    System.out.printf("Random message: %s (%s)%n",l.get(r),r);
+    
+//    System.out.printf("%s%n",arrayList.get(messageNumber));
+//    this.printMessage = null;
   }
   
 }
