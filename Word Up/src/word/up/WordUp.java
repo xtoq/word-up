bc16d3bb2810694c441e89c745e2f65ccd083550
Smokie Lee <xtoq@users.noreply.github.com>
2015-09-05 09:15:07 -0500

    0.3
    - Fixes for the random number always being 0. Not so random after all.

--- Word Up/src/word/up/WordUp.java
+++ Word Up/src/word/up/WordUp.java
@@ -6,21 +6,20 @@
 package word.up;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
  *
  * @author Smokie Lee
  */
 public class WordUp {
-  private ArrayList arrayList;
+  private final ArrayList arrayList;
   private Integer randomNumber;
   private String printMessage;
   
   // constructors
   public WordUp() {
     this.arrayList = new ArrayList<>();
-    this.randomNumber = 0;
+    this.randomNumber = 5;
     this.printMessage = "";
   }
   
@@ -43,24 +42,36 @@ public class WordUp {
   
   // setters
   // setArrayList, setRandomNumber, setPrintMessage
-  public void setArrayList() {
+  public void setArrayList(String message) {
     // set/put values into the array list
+//    arrayList.set(1,"first");
+//    this.arrayList = arrayList.asList("One", "Two", "Three");
+    this.arrayList.add(message);
   }
   
+//public void add(Item i) {
+//
+//    itemsInGroceryList.add(i);
+//}
+  
   public void setRandomNumber() {
+    setRandomNumber(0,arrayList.size());
+  }
+  
+  public void setRandomNumber(Integer a, Integer b) {
     // set random number after computing it
+//    s = arrayList.size();
+    a = 0; // Lower bound is the first value. First index in an array list is always 0.
+    b = this.arrayList.size() - 1; // Upper bound is the size of the array list - 1.
+//    int r = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
+    this.randomNumber = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
+    // int r = (int) (Math.random() * (l.size() + 1)); // Simplified equation. Probably don't want to use this.
   }
   
   public void setPrintMessage(int messageNumber, ArrayList<String> arrayList) {
     // set print message value
     System.out.printf("%s%n",arrayList.get(messageNumber));
     this.printMessage = null;
-    
-//    // print array list in readable format
-//    int order = 1; // Counter for the numbered list.
-//    for (Object message : arrayList) { // Enhanced for loop is more efficient here.
-//      System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
-//    }
   }
   
 //  // other methods
@@ -77,8 +88,8 @@ public class WordUp {
    */
   public static void main(String[] args) {
     // create new object(s)
-//    WordUp w = new WordUp();
-    ArrayList l = new ArrayList();
+    WordUp w = new WordUp();
+    ArrayList l = new ArrayList<>();
 
     // set array list
     l.add("puppy");
@@ -87,26 +98,43 @@ public class WordUp {
     l.add("laser");
     l.add("larvae");
     
-    // generate random number
-    int s = l.size();
-    int a = 0; // Lower bound is the first value. First index in an array list is always 0.
-    int b = s - 1; // Upper bound is the size of the array list - 1.
-    int r = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
-    // int r = (int) (Math.random() * (l.size() + 1)); // Simplified equation. Probably don't want to use this.
+//    w.setArrayList("puppy");
+//    w.setArrayList("kitten");
     
+    // generate random number
+    if (l.size() > 0) {
+//    s = arrayList.size();
+    Integer a = 0; // Lower bound is the first value. First index in an array list is always 0.
+    Integer b = l.size() - 1; // Upper bound is the size of the array list - 1.
+//    int r = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
+    Integer r = (int) (Math.random() * (b - a + 1)) + a; // where a & b are the lower and upper bounds respectively.
+    // int r = (int) (Math.random() * (l.size() + 1)); // Simplified equation. Probably don't want to use this.
+    System.out.printf("Number is: %s%n%n",r);
+//      w.setRandomNumber();
+//      w.getRandomNumber();
+//      Integer r = w.getRandomNumber();
+      
+      System.out.println("RESULTS");
+      System.out.printf("List size is %s.%n",l.size());
+      
+      int order = 1; // Counter for the numbered list.
+  //    int order2 = 0;
+      for (Object message : l) { // Enhanced for loop is more efficient here.
+        System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
+  //      System.out.printf("%s. %s%n",String.valueOf(order2++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
+      }
+      System.out.printf("Random number: %s%n",r);
+      System.out.printf("Random message: %s%n",l.get(r));
+    }
+    else {
+      System.out.println("RESULTS");
+      System.out.printf("List size is %s.%n",l.size());
+    }
     // print array list message
-    System.out.printf("Random message: %s (%s)%n",l.get(r),r);
     
     // debugging
-    System.out.println("RESULTS");
     
-//    int order = 1; // Counter for the numbered list.
-    int order2 = 0;
-    for (Object message : l) { // Enhanced for loop is more efficient here.
-//      System.out.printf("%s. %s%n",String.valueOf(order++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
-      System.out.printf("%s. %s%n",String.valueOf(order2++),message); // We're adding 1 to the order value to make this human-readable since the index starts at 0.
-    }
-    System.out.printf("Random number: %s (%s)%n",r,l.get(r));
+
   }
   
 }
